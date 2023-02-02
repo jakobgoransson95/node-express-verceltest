30414985b9df78e9f9672ef08407003da74b4154
jakob göransson <jakob.goransson@hotmail.com>
2023-02-02 18:26:59 +0100

    test

--- index.js
+++ index.js
@@ -2,12 +2,44 @@
 const express = require("express");
 const home = require("./routes/home");
 
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: 'hattie.db.elephantsql.com',
+    port: 5432,
+    user: 'plcwwdix',
+    password: 'IW2fq8188nBZTNRTdxT-_bKFCwkh2FbR',
+    database: 'plcwwdix'
+  }
+});
+
+
+
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
 app.use("/home", home);
+
+
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('ok');
+})
+
+
+
 
 // connection
 const port = process.env.PORT || 9001;
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
+// const app = express();
+// const bcrypt = require('bcrypt-nodejs');
+// const cors = require('cors');
+// const knex = require('knex');
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: 'hattie.db.elephantsql.com',
+//     port: 5432,
+//     user: 'plcwwdix',
+//     password: 'IW2fq8188nBZTNRTdxT-_bKFCwkh2FbR',
+//     database: 'plcwwdix'
+//   }
+// });
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(cors());
+
+
+// app.get('/', (req, res) => {
+//   res.send('ok');
+// })
+
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
   });
 });
+
+
+
 
 module.exports = router;
