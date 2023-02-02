91c28ed4dbf2c599898ae2104f557e07d3f0aa05
jakob göransson <jakob.goransson@hotmail.com>
2023-02-02 18:30:59 +0100

    test2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/home", home);
 
 
 
-
+///jag har lag till////
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -36,6 +36,19 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('ok');
+})
+
+
+app.post('/test', (req, res) => {
+  db('recept').insert({
+    maträtt: req.body.maträtt,
+  })
+    .then(function () {
+      db.select().from('recept')
+        .then(allMessage => {
+          res.json(allMessage)
+        })
+    })
 })
 
 
