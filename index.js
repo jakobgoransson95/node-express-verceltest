f69dc5fb5d6c1b70121f3eba11d8893041b8e0a8
jakob göransson <jakob.goransson@hotmail.com>
2023-02-11 15:15:51 +0100

    L

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
-
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
